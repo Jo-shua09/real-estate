e0cbfda143c77fd1661332d654514d8ed3d14b49
Jo-shua09 <joshuaonyeka039@gmail.com>
2025-04-14 21:00:51 +0100

    started the last section in the property page

---
+++ src/Pages/Property/sections/Message.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+import SectionIntro from "../../../Components/SectionIntro";
+
+const Message = () => {
+  return (
+    <div className="w-full section">
+      <SectionIntro
+        totle="let's make it happen"
+        description="Ready to take the first step toward your dream property? Fill out the form below, and our real estate wizards will work their magic to find your perfect match. Don't wait; let's embark on this exciting journey together."
+        hideButton={true}
+      />
+      <div className="w-full border-2 border-white/5 rounded-xl p-10"></div>
+    </div>
+  );
+};
+
+export default Message;
