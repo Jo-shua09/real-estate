b78efc95a9e2ae38da0acc4bf541bea044d347da
Jo-shua09 <joshuaonyeka039@gmail.com>
2025-04-14 17:36:41 +0100

    rounding up the about page

--- src/Pages/About/sections/Clients.jsx
+++ src/Pages/About/sections/Clients.jsx
@@ -1,6 +1,9 @@
 import { useEffect, useState } from "react";
 import SwiperSlideLayout from "../../../Components/SwiperSlideLayout";
 import mockData from "../../../assets/mock_data.json";
+import { AppBar } from "@mui/material";
+import { LightRounded } from "@mui/icons-material";
+import { Link } from "react-router-dom";
 
 const Clients = () => {
   const [clients, setClients] = useState([]);
@@ -24,8 +27,45 @@ const Clients = () => {
         navPrevId="prev-button3"
         navNextId="next-button3"
         renderSlide={(client) => (
-          <div className="">
-            <div className="">{client.name}</div>
+          <div className="w-full h-full">
+            <div className="w-full border-[.5rem] border-white/5 rounded-xl p-7">
+              <div className="flex justify-between items-center">
+                <div className="flex flex-col gap-y-2">
+                  <h5 className="text-xl text-white/70 font-medium">
+                    since {client.since}
+                  </h5>
+                  <h2 className="text-3xl font-medium">{client.name}</h2>
+                </div>
+
+                <button className="text-2xl font-medium bg-white/10 border-2 rounded-xl py-3 px-6 border-white/5 capitalize">
+                  <Link to={client.website}>visit website</Link>
+                </button>
+              </div>
+
+              <div className="my-10 flex justify-between w-full items-center">
+                <div className="flex flex-col gap-y-2">
+                  <div className="flex items-center text-white/70 gap-x-2">
+                    <AppBar className="!text-4xl" />
+                    <span className="text-xl font-medium">domain</span>
+                  </div>
+                  <h3 className="text-2xl">{client.domain}</h3>
+                </div>
+
+                <div className="flex flex-col gap-y-2">
+                  <div className="flex items-center text-white/70 gap-x-2">
+                    <LightRounded className="!text-4xl" />
+                    <span className="text-xl font-medium">category</span>
+                  </div>
+                  <h3 className="text-2xl">{client.category}</h3>
+                </div>
+              </div>
+              <div className="w-full p-7 rounded-xl border-2 border-white/5">
+                <h3 className="text-xl text-white/70">what they said</h3>
+                <p className="text-2xl font-medium normal-case">
+                  {client.testimonial}
+                </p>
+              </div>
+            </div>
           </div>
         )}
       />
