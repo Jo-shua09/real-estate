d3714777e94893eaa81bf3077d17d90e05c4d8f1
Jo-shua09 <joshuaonyeka039@gmail.com>
2025-04-15 22:13:43 +0100

    doing some testing

--- src/Pages/Property/sections/SearchResult.jsx
+++ src/Pages/Property/sections/SearchResult.jsx
@@ -36,52 +36,57 @@ const SearchResult = ({ properties = [] }) => {
     <div className="w-full h-full">
       {properties.length < 3 ? (
         // Single property view
-        <div className="max-w-4xl mt-10 flex items-center justify-center gap-5 md:flex-nowrap flex-wrap mx-auto">
-          <h3 className="text-3xl font-semibold">search results</h3>
-          {properties.map((result) => (
-            <div key={result.property_id} className="border-2 border-white/5 rounded-xl p-6 w-full h-fit shadow-xl hover:shadow-xl transition-all">
-              <div className="">
-                <img src={result.image_url} alt={result.property_name} className="w-full h-[25rem] object-cover rounded-xl" />
+        <div className="flex flex-col mt-10 gap-y-5 justify-center items-center">
+          <h3 className="text-3xl font-semibold"> your search results</h3>
+          <div className="max-w-4xl flex items-center justify-center gap-5 md:flex-nowrap flex-wrap mx-auto">
+            {properties.map((result) => (
+              <div key={result.property_id} className="border-2 border-white/5 rounded-xl p-6 w-full h-fit shadow-xl hover:shadow-xl transition-all">
+                <div className="">
+                  <img src={result.image_url} alt={result.property_name} className="w-full h-[25rem] object-cover rounded-xl" />
+                </div>
+
+                <div className="mt-4">
+                  <h3 className="text-3xl font-semibold">{result.property_name}</h3>
+                  <div className="my-3">
+                    <p className={`text-2xl normal-case text-white/70 overflow-hidden ${!expandedCards[result.property_id] ? "line-clamp-2" : ""}`}>
+                      {result.description}
+                    </p>
+                    <span
+                      onClick={() => handleShow(result.property_id)}
+                      className="font-medium text-2xl text-white cursor-pointer underline hover:no-underline"
+                    >
+                      {expandedCards[result.property_id] ? "show less" : "read more"}
+                    </span>
+                  </div>
+
+                  <div className="flex justify-between gap-x-2 my-6 items-center w-full">
+                    <div className="flex-1 rounded-full px-2 py-2 flex gap-x-2 bg-white/5 border-white/5 border-2 items-center justify-center">
+                      <BedRounded className="!text-5xl" />
+                      <span className="text-xl text-nowrap font-semibold flex items-center">{result.bedrooms}-bedrooms</span>
+                    </div>
+                    <div className="flex-1 rounded-full px-2 py-2 flex gap-x-2 bg-white/5 border-white/5 border-2 items-center justify-center">
+                      <Bathtub className="!text-5xl" />
+                      <span className="text-xl text-nowrap font-semibold flex items-center">{result.bathrooms}-bathrooms</span>
+                    </div>
+                    <div className="flex-1 rounded-full px-2 py-2 flex gap-x-2 bg-white/5 border-white/5 border-2 items-center justify-center">
+                      <Villa className="!text-5xl" />
+                      <span className="text-xl text-nowrap font-semibold flex items-center">{result.property_type}</span>
+                    </div>
+                  </div>
+
+                  <div className="flex justify-between items-center mt-10 w-full">
+                    <div className="flex flex-col gap-y-2">
+                      <span className="text-xl text-white/70">price</span>
+                      <p className="text-3xl font-semibold">${result.price.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",")}</p>
+                    </div>
+                    <div onClick={() => handlePropertyClick(result)}>
+                      <Button name="view property details" />
+                    </div>
+                  </div>
+                </div>
               </div>
-
-              <div className="mt-4">
-                <h3 className="text-3xl font-semibold">{result.property_name}</h3>
-                <div className="my-3">
-                  <p className={`text-2xl normal-case text-white/70 overflow-hidden ${!expandedCards[result.property_id] ? "line-clamp-2" : ""}`}>
-                    {result.description}
-                  </p>
-                  <span onClick={() => handleShow(result.property_id)} className="font-medium text-2xl text-white cursor-pointer underline hover:no-underline">
-                    {expandedCards[result.property_id] ? "show less" : "read more"}
-                  </span>
-                </div>
-
-                <div className="flex justify-between gap-x-2 my-6 items-center w-full">
-                  <div className="flex-1 rounded-full px-2 py-2 flex gap-x-2 bg-white/5 border-white/5 border-2 items-center justify-center">
-                    <BedRounded className="!text-5xl" />
-                    <span className="text-xl text-nowrap font-semibold flex items-center">{result.bedrooms}-bedrooms</span>
-                  </div>
-                  <div className="flex-1 rounded-full px-2 py-2 flex gap-x-2 bg-white/5 border-white/5 border-2 items-center justify-center">
-                    <Bathtub className="!text-5xl" />
-                    <span className="text-xl text-nowrap font-semibold flex items-center">{result.bathrooms}-bathrooms</span>
-                  </div>
-                  <div className="flex-1 rounded-full px-2 py-2 flex gap-x-2 bg-white/5 border-white/5 border-2 items-center justify-center">
-                    <Villa className="!text-5xl" />
-                    <span className="text-xl text-nowrap font-semibold flex items-center">{result.property_type}</span>
-                  </div>
-                </div>
-
-                <div className="flex justify-between items-center mt-10 w-full">
-                  <div className="flex flex-col gap-y-2">
-                    <span className="text-xl text-white/70">price</span>
-                    <p className="text-3xl font-semibold">${result.price.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",")}</p>
-                  </div>
-                  <div onClick={() => handlePropertyClick(result)}>
-                    <Button name="view property details" />
-                  </div>
-                </div>
-              </div>
-            </div>
-          ))}
+            ))}
+          </div>
         </div>
       ) : (
         // Multiple properties view with Swiper
