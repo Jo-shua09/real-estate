2c578548b730827fbe729a5b55a4c8d26f6cf89c
Jo-shua09 <joshuaonyeka039@gmail.com>
2025-04-16 23:35:08 +0100

    started the pricing details section

--- src/Pages/Property_details/PropertyDetails.jsx
+++ src/Pages/Property_details/PropertyDetails.jsx
@@ -1,9 +1,13 @@
+import FAQ from "../Home/sections/FAQ";
 import Inquire from "./sections/Inquire";
+import PricingDetails from "./sections/PricingDetails";
 
 const PropertyDetails = () => {
   return (
     <>
       <Inquire />
+      <PricingDetails />
+      <FAQ />
     </>
   );
 };
