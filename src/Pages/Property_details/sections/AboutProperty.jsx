2b90de164a30b787f13d8bb8ab450aebe715208d
Jo-shua09 <joshuaonyeka039@gmail.com>
2025-04-18 00:55:49 +0100

    starting of the property details page

--- src/Pages/Property_details/sections/AboutProperty.jsx
+++ src/Pages/Property_details/sections/AboutProperty.jsx
@@ -1,13 +1,26 @@
+import { LocationOn } from "@mui/icons-material";
 import { useLocation } from "react-router-dom";
 
 const AboutProperty = () => {
   const Location = useLocation();
-  const { property_name } = Location.state || [];
+  const { property_name, location, price } = Location.state || [];
 
   return (
     <div className="w-full section-page">
-      <h2 className="text-4xl font-semibold py-5">{property_name}</h2>
-      <div className=""></div>
+      <h2 className="text-4xl font-semibold py-3">{property_name}</h2>
+      <div className="flex items-center justify-between w-full gap-10">
+        <div className="flex items-center gap-x-2">
+          <LocationOn className="!text-4xl" />
+          <span className="text-2xl font-medium text-white/70">{location}</span>
+        </div>
+        <div className="text-3xl font-semibold">
+          <h5 className="text-xl text-white/70 font-medium">price</h5>${price.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",")}
+        </div>
+      </div>
+
+      <div className="w-full p-10 md:p-7 border-2 border-white/5 bg-white/5 my-10 rounded-xl">
+        <div className=""></div>
+      </div>
     </div>
   );
 };
