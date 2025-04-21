73a7645e0fc7acbf023b047dbdd26590ebb3407d
Jo-shua09 <joshuaonyeka039@gmail.com>
2025-04-22 00:12:59 +0100

    still here

--- src/Pages/Services/sections/Investment.jsx
+++ src/Pages/Services/sections/Investment.jsx
@@ -3,22 +3,25 @@ import SectionIntro from "../../../Components/SectionIntro";
 
 const Investment = () => {
   return (
-    <div className="w-full h-full section flex items-center justify-between gap-7">
+    <div className="w-full h-full section flex justify-between gap-7">
       <div className="flex flex-col gap-7 w-full flex-1">
         <SectionIntro
+          hideButton={true}
           title="Smart Investments, Informed Decisions"
           description="Building a real estate portfolio requires a strategic approach. Estatein's Investment Advisory Service empowers you to make smart investments and informed decisions."
         />
+
         <div className="bg-hero p-10 rounded-xl bg-white/5 flex flex-col gap-y-6">
-          <h3 className="text-3xl font-semibold">Unlock Your Investment Potential</h3>
-          <p className="text-white/70 font-medium normal-case">
+          <h3 className="text-4xl font-semibold">Unlock Your Investment Potential</h3>
+          <p className="text-white/70 font-medium normal-case text-2xl">
             Explore our Property Management Service categories and let us handle the complexities while you enjoy the benefits of property ownership.
           </p>
-          <button className="py-4  px-7 hover:scale-95 capitalize rounded-xl  md:hidden w-full h-[5rem] justify-center flex items-center font-semibold bg-black/35 text-2xl border-2 border-white/5">
+          <button className="py-4  px-7 hover:scale-95 capitalize rounded-xl w-full h-[5rem] justify-center flex items-center font-semibold bg-black/35 text-2xl border-2 border-white/5">
             learn more
           </button>
         </div>
       </div>
+      <div className="w-full flex-[2] bg-white/5 p-5 rounded-xl"></div>
     </div>
   );
 };
