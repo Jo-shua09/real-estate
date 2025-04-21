e22e677dbe2caa0cebd0439eff42c247fefc585b
Jo-shua09 <joshuaonyeka039@gmail.com>
2025-04-22 00:06:27 +0100

    here now

---
+++ src/Pages/Services/sections/Investment.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+import SectionIntro from "../../../Components/SectionIntro";
+
+const Investment = () => {
+  return (
+    <div className="w-full h-full section flex items-center justify-between gap-7">
+      <div className="flex flex-col gap-7 w-full flex-1">
+        <SectionIntro />
+        <div className=""></div>
+      </div>
+    </div>
+  );
+};
+
+export default Investment;
