e80867b441908a6dcb9f4eb5d1c8bc7517bd1e5d
Jo-shua09 <joshuaonyeka039@gmail.com>
2025-04-22 00:09:05 +0100

    still here

--- src/Pages/Services/sections/Investment.jsx
+++ src/Pages/Services/sections/Investment.jsx
@@ -9,7 +9,15 @@ const Investment = () => {
           title="Smart Investments, Informed Decisions"
           description="Building a real estate portfolio requires a strategic approach. Estatein's Investment Advisory Service empowers you to make smart investments and informed decisions."
         />
-        <div className=""></div>
+        <div className="bg-hero p-10 rounded-xl bg-white/5 flex flex-col gap-y-6">
+          <h3 className="text-3xl font-semibold">Unlock Your Investment Potential</h3>
+          <p className="text-white/70 font-medium normal-case">
+            Explore our Property Management Service categories and let us handle the complexities while you enjoy the benefits of property ownership.
+          </p>
+          <button className="py-4  px-7 hover:scale-95 capitalize rounded-xl  md:hidden w-full h-[5rem] justify-center flex items-center font-semibold bg-black/35 text-2xl border-2 border-white/5">
+            learn more
+          </button>
+        </div>
       </div>
     </div>
   );
