6c4be5e7556ad02a8a046bceebacdef354f4dd7b
Jo-shua09 <joshuaonyeka039@gmail.com>
2025-04-22 00:06:57 +0100

    still heree

--- src/Pages/Services/sections/Investment.jsx
+++ src/Pages/Services/sections/Investment.jsx
@@ -5,7 +5,10 @@ const Investment = () => {
   return (
     <div className="w-full h-full section flex items-center justify-between gap-7">
       <div className="flex flex-col gap-7 w-full flex-1">
-        <SectionIntro />
+        <SectionIntro
+          title="Smart Investments, Informed Decisions"
+          description="Building a real estate portfolio requires a strategic approach. Estatein's Investment Advisory Service empowers you to make smart investments and informed decisions."
+        />
         <div className=""></div>
       </div>
     </div>
