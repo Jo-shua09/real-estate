7023894a0035967d2e167e036a790f3714c9c45e
Jo-shua09 <joshuaonyeka039@gmail.com>
2025-04-22 01:27:19 +0100

    all set

--- src/Pages/Contact/sections/OurOffice.jsx
+++ src/Pages/Contact/sections/OurOffice.jsx
@@ -23,16 +23,16 @@ const OurOffice = () => {
           </button>
         </div>
 
-        <div className="flex mt-10 flex-wrap md:flex-nowrap items-center justify-between gap-10 w-full">
+        <div className="flex mt-5 flex-wrap md:flex-nowrap items-center justify-between gap-10 w-full">
           <div className="md:flex-1 w-full rounded-xl border-2 border-white/5 p-10">
             <h4 className="text-xl normal-case text-white/70">Main Headquarters</h4>
-            <h3 className="text-4xl py-7 font-semibold">123 Estantien Plaza, City Center, Metropolis</h3>
+            <h3 className="text-4xl md:text-3xl py-7 font-semibold">123 Estantien Plaza, City Center, Metropolis</h3>
             <p className="text-2xl font-medium normal-case text-white/70">
               Our main headquarters serve as the heart of Estantien. Located in the bustling city center, this is where our core team of experts operates,
               driving the excellence and innovation that define us.
             </p>
 
-            <div className="flex my-12 justify-between gap-x-2 items-center w-full">
+            <div className="flex my-12 justify-between gap-x-2 gap-y-4 items-center w-full flex-wrap">
               <div className="flex-1 rounded-full px-2 py-2 flex gap-x-2 bg-white/5 border-white/5 border-2 items-center justify-center">
                 <Mail className="!text-5xl" />
                 <span className="text-xl text-nowrap font-semibold flex items-center">info@Estantien.com</span>
@@ -54,13 +54,13 @@ const OurOffice = () => {
 
           <div className="md:flex-1 w-full rounded-xl border-2 border-white/5 p-10">
             <h4 className="text-xl normal-case text-white/70">Regional Offices</h4>
-            <h3 className="text-4xl py-7 font-semibold">456 Urban Avenue, Downtown District, Metropolis</h3>
+            <h3 className="text-4xl md:text-3xl py-7 font-semibold">456 Urban Avenue, Downtown District, Metropolis</h3>
             <p className="text-2xl font-medium normal-case text-white/70">
               Estantien's presence extends to multiple regions, each with its own dynamic real estate landscape. Discover our regional offices, staffed by local
               experts who understand the nuances of their respective markets.
             </p>
 
-            <div className="flex my-12 justify-between gap-x-2 items-center w-full">
+            <div className="flex my-12 justify-between gap-x-2 gap-y-4 items-center w-full flex-wrap">
               <div className="flex-1 rounded-full px-2 py-2 flex gap-x-2 bg-white/5 border-white/5 border-2 items-center justify-center">
                 <Mail className="!text-5xl" />
                 <span className="text-xl text-nowrap font-semibold flex items-center">info@Estantien.com</span>
