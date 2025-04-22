c7d4db7b4ba561cceb4874cf2c9fbf66dab86751
Jo-shua09 <joshuaonyeka039@gmail.com>
2025-04-22 01:57:36 +0100

    still here

--- src/Pages/Contact/sections/Gallery.jsx
+++ src/Pages/Contact/sections/Gallery.jsx
@@ -9,18 +9,18 @@ import gallery5 from "/src/assets/Images/Contact/gallery5.jpg";
 const Gallery = () => {
   return (
     <div className="w-full section mt-10">
-      <div className="bg-hero bg-white/5 p-10 rounded-xl">
-        <div className="grid-cols-2 grid items-center justify-center gap-10">
-          <img src={gallery1} alt="gallery image" className="w-full h-full object-cover rounded-xl" />
-          <img src={gallery2} alt="gallery image" className="w-full h-full object-cover rounded-xl" />
-          <img src={gallery3} alt="gallery image" className="w-full h-full object-cover rounded-xl" />
-          <div className="flex items-center gap-x-3 w-full">
-            <img src={gallery4} alt="gallery image" className="w-full h-full object-cover rounded-xl" />
-            <img src={gallery2} alt="gallery image" className="w-full h-full object-cover rounded-xl" />
+      <div className="bg-hero bg-white/5 md:p-20 p-10 rounded-xl">
+        <div className="grid-cols-2 grid items-center justify-center gap-7">
+          <img src={gallery1} alt="gallery image" className="w-full md:h-full h-[14rem] object-cover rounded-xl" />
+          <img src={gallery2} alt="gallery image" className="w-full md:h-full h-[14rem] object-cover rounded-xl" />
+          <img src={gallery3} alt="gallery image" className="w-full md:h-full h-[14rem] object-cover rounded-xl" />
+          <div className="grid grid-cols-2 items-center gap-x-3 w-full">
+            <img src={gallery4} alt="gallery image" className="w-full md:h-full h-[14rem] object-cover rounded-xl" />
+            <img src={gallery2} alt="gallery image" className="w-full md:h-full h-[14rem] object-cover rounded-xl" />
           </div>
         </div>
 
-        <div className="flex items-center flex-wrap md:flex-nowrap">
+        <div className="flex items-center flex-wrap md:flex-nowrap mt-7">
           <div className="w-full py-10 md:flex-1">
             <SectionIntro
               title="Explore Estatein's World"
