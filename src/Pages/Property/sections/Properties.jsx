12e526f1708cbd2cde0ac1f7a998a531af7956e9
Jo-shua09 <joshuaonyeka039@gmail.com>
2025-04-23 13:07:39 +0100

    refactor(Properties): restructure layout and add SectionIntro component
    Refactored the Properties component to improve maintainability by introducing the SectionIntro component. Additionally, restructured the layout to conditionally render content based on the current page and results length. This change enhances code readability and modularity.

--- src/Pages/Property/sections/Properties.jsx
+++ src/Pages/Property/sections/Properties.jsx
@@ -5,6 +5,7 @@ import mockData from "../../../assets/mock_data.json";
 import { useNavigate } from "react-router-dom";
 import { Bathtub, BedRounded, Villa } from "@mui/icons-material";
 import { Button } from "../../../Components/general/Button";
+import SectionIntro from "../../../Components/SectionIntro";
 
 const Properties = () => {
   const [results, setResults] = useState([]);
@@ -43,80 +44,102 @@ const Properties = () => {
     });
   };
 
+  console.log(results.length);
+  console.log(allProperties.length);
+  console.log(currentPage);
   return (
     <div className="w-full h-full">
       <div className="">
-        <SwiperSlideLayout
-          items={results}
-          title="discover a world of possibilities"
-          hideButton={true}
-          disableAt={2}
-          description="Our portfolio of properties is as diverse as your dreams. Explore the following categories to find the perfect property that resonates with your vision of home"
-          breakpoints={{
-            400: { slidesPerView: 1 },
-            768: { slidesPerView: 2 },
-            1024: { slidesPerView: 3 },
-          }}
-          useWFit={true}
-          sliceRange={[]}
-          navPrevId="prev-button3"
-          navNextId="next-button3"
-          renderSlide={(result) => (
-            <div className="border-2 border-white/5 rounded-xl p-6 w-full h-fit shadow-lg hover:shadow-xl transition-all">
-              <div className="">
-                <img src={result.image_url} alt={result.property_name} className="w-full h-[25rem] object-cover rounded-xl" />
-              </div>
-
-              <div className="mt-4">
-                <h3 className="text-3xl font-semibold">{result.property_name}</h3>
-                <div className="my-3">
-                  <p className={`text-2xl normal-case text-white/70 overflow-hidden ${!expandedDescriptions[result.property_id] ? "line-clamp-2" : ""}`}>
-                    {result.description}
-                  </p>
-                  <span
-                    onClick={() => toggleDescription(result.property_id)}
-                    className="font-medium text-2xl text-white cursor-pointer underline hover:no-underline"
-                  >
-                    {expandedDescriptions[result.property_id] ? "show less" : "read more"}
-                  </span>
-                </div>
-
-                <div className="flex justify-between gap-x-2 my-6 items-center w-full">
-                  <div className="flex-1 rounded-full px-2 py-2 flex gap-x-2 bg-white/5 border-white/5 border-2 items-center justify-center">
-                    <BedRounded className="!text-5xl" />
-                    <span className="text-xl text-nowrap font-semibold flex items-center">{result.bedrooms}-bedrooms</span>
-                  </div>
-                  <div className="flex-1 rounded-full px-2 py-2 flex gap-x-2 bg-white/5 border-white/5 border-2 items-center justify-center">
-                    <Bathtub className="!text-5xl" />
-                    <span className="text-xl text-nowrap font-semibold flex items-center">{result.bathrooms}-bathrooms</span>
-                  </div>
-                  <div className="flex-1 rounded-full px-2 py-2 flex gap-x-2 bg-white/5 border-white/5 border-2 items-center justify-center">
-                    <Villa className="!text-5xl" />
-                    <span className="text-xl text-nowrap font-semibold flex items-center">{result.property_type}</span>
-                  </div>
-                </div>
-
-                <div className="flex justify-between items-center mt-10 w-full">
-                  <div className="flex flex-col gap-y-2">
-                    <span className="text-xl text-white/70">price</span>
-                    <p className="text-3xl font-semibold">${result.price.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",")}</p>
-                  </div>
-                  <div onClick={() => handlePropertyClick(result)}>
-                    <Button name="view property details" />
-                  </div>
-                </div>
-              </div>
+        {results.length === currentPage ? (
+          <div className="section-page mt-10">
+            <SectionIntro
+              title="discover a world of possibilities"
+              description="Our portfolio of properties is as diverse as your dreams. Explore the following categories to find the perfect property that resonates with your vision of home"
+              hideButton={true}
+            />
+            <div className="w-full flex justify-center mt-10 flex-col gap-y-5">
+              <h3 className="text-3xl font-semibold text-center">more properties coming soon!</h3>
+              {/* <p className="text-xl font-semibold italic normal-case text-center">"click to view available properties"</p> */}
             </div>
-          )}
-        />
+          </div>
+        ) : (
+          <SwiperSlideLayout
+            items={results}
+            title="discover a world of possibilities"
+            hideButton={true}
+            disableAt={2}
+            description="Our portfolio of properties is as diverse as your dreams. Explore the following categories to find the perfect property that resonates with your vision of home"
+            breakpoints={{
+              400: { slidesPerView: 1 },
+              768: { slidesPerView: 2 },
+              1024: { slidesPerView: 3 },
+            }}
+            useWFit={true}
+            sliceRange={[]}
+            navPrevId="prev-button3"
+            navNextId="next-button3"
+            renderSlide={(result) => (
+              <div className="border-2 border-white/5 rounded-xl p-6 w-full h-fit shadow-lg hover:shadow-xl transition-all">
+                <div className="">
+                  <img src={result.image_url} alt={result.property_name} className="w-full h-[25rem] object-cover rounded-xl" />
+                </div>
 
-        {results.length === allProperties.length ? (
-          <div className="w-full flex justify-center mt-10">
-            <h3 className="text-3xl font-semibold text-center text-white/70">more properties coming soon!</h3>
+                <div className="mt-4">
+                  <h3 className="text-3xl font-semibold">{result.property_name}</h3>
+                  <div className="my-3">
+                    <p className={`text-2xl normal-case text-white/70 overflow-hidden ${!expandedDescriptions[result.property_id] ? "line-clamp-2" : ""}`}>
+                      {result.description}
+                    </p>
+                    <span
+                      onClick={() => toggleDescription(result.property_id)}
+                      className="font-medium text-2xl text-white cursor-pointer underline hover:no-underline"
+                    >
+                      {expandedDescriptions[result.property_id] ? "show less" : "read more"}
+                    </span>
+                  </div>
+
+                  <div className="flex justify-between gap-x-2 my-6 items-center w-full">
+                    <div className="flex-1 rounded-full px-2 py-2 flex gap-x-2 bg-white/5 border-white/5 border-2 items-center justify-center">
+                      <BedRounded className="!text-5xl" />
+                      <span className="text-xl text-nowrap font-semibold flex items-center">{result.bedrooms}-bedrooms</span>
+                    </div>
+                    <div className="flex-1 rounded-full px-2 py-2 flex gap-x-2 bg-white/5 border-white/5 border-2 items-center justify-center">
+                      <Bathtub className="!text-5xl" />
+                      <span className="text-xl text-nowrap font-semibold flex items-center">{result.bathrooms}-bathrooms</span>
+                    </div>
+                    <div className="flex-1 rounded-full px-2 py-2 flex gap-x-2 bg-white/5 border-white/5 border-2 items-center justify-center">
+                      <Villa className="!text-5xl" />
+                      <span className="text-xl text-nowrap font-semibold flex items-center">{result.property_type}</span>
+                    </div>
+                  </div>
+
+                  <div className="flex justify-between items-center mt-10 w-full">
+                    <div className="flex flex-col gap-y-2">
+                      <span className="text-xl text-white/70">price</span>
+                      <p className="text-3xl font-semibold">${result.price.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",")}</p>
+                    </div>
+                    <div onClick={() => handlePropertyClick(result)}>
+                      <Button name="view property details" />
+                    </div>
+                  </div>
+                </div>
+              </div>
+            )}
+          />
+        )}
+
+        {results.length === currentPage ? (
+          <div className="w-full justify-center flex mt-10">
+            <button
+              onClick={() => window.location.reload()}
+              className="bg-purple-600 capitalize hover:scale-95 text-white font-bold py-4 px-6 rounded-lg text-2xl"
+            >
+              view available properties
+            </button>
           </div>
         ) : (
           <div className="w-full flex justify-center mt-10">
-            <button onClick={loadMore} className="bg-purple-600 hover:scale-95 text-white font-bold py-4 px-6 rounded-lg text-2xl">
+            <button onClick={loadMore} className="bg-purple-600 capitalize hover:scale-95 text-white font-bold py-4 px-6 rounded-lg text-2xl">
               Load More Properties
             </button>
           </div>
